50e6524c990d3c4cc9dbf232eb88982cdb392042
Ricardo de Paula <r.cap@hotmail.com>
2021-11-04 16:53:20 -0300

    add

--- svelte.config.js
+++ svelte.config.js
@@ -1,59 +1,9 @@
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// options passed to svelte.compile (https://svelte.dev/docs#svelte_compile)
-	compilerOptions: null,
-
-	// an array of file extensions that should be treated as Svelte components
-	extensions: ['.svelte'],
-
 	kit: {
-		adapter: null,
-		amp: false,
-		appDir: '_app',
-		files: {
-			assets: 'static',
-			hooks: 'src/hooks',
-			lib: 'src/lib',
-			routes: 'src/routes',
-			serviceWorker: 'src/service-worker',
-			template: 'src/app.html'
-		},
-		floc: false,
-		host: null,
-		hostHeader: null,
-		hydrate: true,
-		package: {
-			dir: 'package',
-			emitTypes: true,
-			// excludes all .d.ts and files starting with _ as the name
-			exports: (filepath) => !/^_|\/_|\.d\.ts$/.test(filepath),
-			files: () => true
-		},
-		paths: {
-			assets: '',
-			base: ''
-		},
-		prerender: {
-			crawl: true,
-			enabled: true,
-			entries: ['*'],
-			onError: 'fail'
-		},
-		router: true,
-		serviceWorker: {
-			files: (filepath) => !/\.DS_STORE/.test(filepath)
-		},
-		ssr: true,
-		target: null,
-		trailingSlash: 'never',
-		vite: () => ({})
-	},
-	
-	// SvelteKit uses vite-plugin-svelte. Its options can be provided directly here.
-	// See the available options at https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/config.md
-
-	// options passed to svelte.preprocess (https://svelte.dev/docs#svelte_preprocess)
-	preprocess: null
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
 };
 
 export default config;

dfdf178f69d25713e671754e82339aa5d3bbdcd4
Ricardo de Paula <r.cap@hotmail.com>
2021-11-07 13:48:40 -0300

    form layout

--- src/routes/contato.svelte
+++ src/routes/contato.svelte
@@ -16,18 +16,21 @@
 	<form name="contact" method="post" netlify netlify-honeypot="bot-field">
 		<input type="hidden" name="form-name" value="contact" />
 		<!-- <input type="text" name="bot-field" /> -->
-		<p>
-			<label>Seu Nome: <input type="text" name="name" /></label>
-		</p>
-		<p>
-			<label>Seu Email: <input type="email" name="email" /></label>
-		</p>
-		<p>
-			<label>Mensagem: <textarea name="message" /></label>
-		</p>
-		<p>
-			<button type="submit">Send</button>
-		</p>
+		<div class="form">
+			<div class="name">
+				<label>Seu Nome: <input type="text" name="name" /></label>
+			</div>
+			<div class="email">
+				<label>Seu Email: <input type="email" class="ml" name="email" /></label>
+			</div>
+			<div class="message">
+				<label>Mensagem: <textarea name="message" /></label>
+
+				<p>
+					<button type="submit">Enviar</button>
+				</p>
+			</div>
+		</div>
 	</form>
 </div>
 
@@ -36,11 +39,40 @@
 		max-width: var(--max-width);
 		margin: 62px auto;
 	}
+	.form {
+		display: grid;
+		grid-template-columns: 300px 300px;
+	}
+	.name {
+		grid-column: 1 / 2;
+	}
+	.email {
+		grid-column: 2 / 3;
+	}
+	.message {
+		grid-column: 1 / 3;
+	}
 
 	@media (max-width: 768px) {
 		.container {
 			padding: 0px 20px;
-			text-align: center;
+			text-align: left;
+		}
+
+		.form {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+		}
+
+		.ml {
+			margin-left: 3px;
+		}
+		.email {
+			margin-top: 20px;
+		}
+		.message {
+			margin-top: 20px;
 		}
 	}
 </style>
